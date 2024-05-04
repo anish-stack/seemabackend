253d6eedce85df9197dfc8f8569a63ed037bfb25
Anish jha <122715402+anish-stack@users.noreply.github.com>
2024-05-04 14:38:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const ConnectDB = require('./config/Db')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-  origin: ['http://localhost:5173','http://localhost:5174','http://localhost:3000','naturalcottoncollection.com'],
+  origin: ['http://localhost:5173','http://localhost:5174','http://localhost:3000','naturalcottoncollection.com','www.naturalcottoncollection.com'],
   methods: 'GET,PUT,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true
